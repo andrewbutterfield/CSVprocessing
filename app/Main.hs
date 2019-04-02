312e77165e7d2cf63222b842dec1f1340a9d5570
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2019-04-02 21:08:10 +0100

    key CSV and Map dependencies sorted

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import CSVFiles
 
 main :: IO ()
 main = someFunc
---
+++ src/CSVFiles.hs
@@ -0,0 +1,10 @@
+module CSVFiles
+    ( someFunc
+    ) where
+
+import Text.CSV
+import Data.Map (Map)
+import qualified Data.Map as M
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
